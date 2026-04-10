1905003bef1d0303f46571caeeb6f135361c3c32
arkadacasinos <bezdep.rf@mail.ru>
2026-04-11 01:22:09 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,16 @@
 import { NextResponse } from 'next/server';
 export function middleware(req) {
-  const country = req.headers.get('x-vercel-ip-country') || 'RU';
+  const url = req.nextUrl;
+  const country = req.headers.get('x-vercel-ip-country') || 'US';
+  const ua = req.headers.get('user-agent') || '';
   const allowedCountries = ['RU', 'BY', 'KZ', 'AM', 'AZ', 'KG', 'TJ', 'UZ', 'MD'];
-  if (!allowedCountries.includes(country)) {
-    return new NextResponse('Access Denied: Your region is not supported.', { status: 403 });
+  const isSuspiciousBot = /HeadlessChrome|Lighthouse|PageSpeed|SemrushBot|AhrefsBot|DotBot|PetalBot/i.test(ua);
+  if (!allowedCountries.includes(country) || isSuspiciousBot) {
+    return new NextResponse(null, { status: 403 });
   }
+
   return NextResponse.next();
 }
+export const config = {
+  matcher: '/:path*',
+};
